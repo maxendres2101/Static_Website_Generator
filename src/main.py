56784bc143bf5d057259b5b899a9688c3905f308
Max Endres <max.endres@conviertemas.com>
2026-03-04 16:13:59 -0500

    changed main to use default basepath

--- src/main.py
+++ src/main.py
@@ -12,11 +12,14 @@ dir_path_static = "./static"
 dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
+default_basepath = "/"
 
 def main():
-    basepath = sys.argv[1]
-    if basepath == '':
-        basepath = '/'
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
+    
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
